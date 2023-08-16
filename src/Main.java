9fbbd0a025acaa86c1987a7b460b042978e4e17a
Naim <skylike2904@gmail.com>
2023-08-16 23:53:05 +0600

    upp

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,44 @@
+import java.util.Scanner;
 
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] arg) {
+		// swapping two numbers:
+//		System.out.println("hello world");
+//		
+//		int n1 = 12;
+//		int n2 = 10;
+//		
+//		System.out.println("n1 = " + n1 + "\nn2 = " + n2);
+//		
+//		int tmp = n1;
+//		n1 = n2; n2 = tmp;
+//		
+//		System.out.println("n1 = " + n1 + "\nn2 = " + n2);
+		
+		
+		
+		// user input:
+//		Scanner scan = new Scanner(System.in);
+//		
+//		System.out.println("What's today?");
+//		String name = scan.nextLine();
+//		
+//		System.out.println("What's your age?");
+//		int age = scan.nextInt();
+//		scan.nextLine();
+//		
+//		System.out.println("What do you like?");
+//		String liking = scan.nextLine();
+//		
+//		
+//		System.out.println(name + " is a pretty good day!");
+//		System.out.println("You're " + age + " years old?!");
+//		System.out.println("Oh! come on, you definately don't like " + liking);
+		
+		
+		
+		//
 	}
 
 }
